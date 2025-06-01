94960ecc77405fe0862cd8bf726100ec6f15c2af
Akshay Rathee <akshay@cartoq.com>
2025-06-02 00:49:03 +0530

    add isPalindrome function to check for palindromes

--- testing.js
+++ testing.js
@@ -3,3 +3,10 @@ function reverseString(str) {
   return str.split('').reverse().join('');
 }
 console.log(reverseString(string));
+
+function isPalindrome(str) {
+  const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  const reversedStr = cleanedStr.split('').reverse().join('');
+  return cleanedStr === reversedStr;
+}
+console.log(isPalindrome(string));
